3a9ee7b335857db039c5354e25b2f24826887d93
Valentyn Medvediuk <medvedikwalle@gmail.com>
2024-12-07 15:32:40 +0100

    add postgresql db

--- src/test/java/com/valikmed/database/DatabaseApplicationTests.java
+++ src/test/java/com/valikmed/database/DatabaseApplicationTests.java
@@ -3,7 +3,7 @@ package com.valikmed.database;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DatabaseApplication.class)
 class DatabaseApplicationTests {
 
 	@Test
