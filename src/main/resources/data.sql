69b362cb27c0b25a3b841ac6ea53530aa67ced79
Valentyn Medvediuk <medvedikwalle@gmail.com>
2024-12-13 20:35:39 +0100

    add db config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets (id, name,purpose) VALUES
+(1,'Widget A', 'Constraint for test purpose'),
+(2,'Widget B', 'Constraint for test purpose'),
+(3,'Widget C', 'Constraint for test purpose'),
+(4,'Widget D', 'Constraint for test purpose'),
+(5,'Widget E', 'Constraint for test purpose');
